61baa6c9f1de4d7204ee7ee03d32a8b9a3f799bd
JeongwonByun <jwbyun0629@naver.com>
2021-12-08 16:30:19 +0900

    upload file

---
+++ BingoServer/BingoServer/GameManager.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace BingoServer
+{
+    class GameManager
+    {
+        public static void copyBingo(string USER) // 유저 빙고 복사
+        {
+            Program.playerA_bingoCount = 0;
+            Program.playerB_bingoCount = 0;
+
+            if (USER == Program.userList[0])
+            {
+                string msg1 = Program.saveUserBingo[Program.userList[0]]; 
+                string msg2 = Program.saveUserBingo[Program.userList[1]]; 
+
+                Program.playerA = Program.userList[0];
+                Program.playerB = Program.userList[1];
+
+                string[] MsgBingo1 = msg1.Split(','); //메세지 분리
+                string[] MsgBingo2 = msg2.Split(',');
+
+                int count = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        // 빙고판에 각 유저의 빙고판 복사
+                        Program.bingoA[i, j] = Convert.ToInt32(MsgBingo1[count]);
+                        Program.bingoB[i, j] = Convert.ToInt32(MsgBingo2[count]);
+                        count++;
+                    }
+                }
+            }
+            else
+            {
+                string msg1 = Program.saveUserBingo[Program.userList[1]]; 
+                string msg2 = Program.saveUserBingo[Program.userList[0]]; 
+
+                Program.playerA = Program.userList[1];
+                Program.playerB = Program.userList[0];
+
+                string[] MsgBingo1 = msg1.Split(','); // 구분자로 메세지 분리
+                string[] MsgBingo2 = msg2.Split(',');
+
+                int count = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        Program.bingoA[i, j] = Convert.ToInt32(MsgBingo1[count]);
+                        Program.bingoB[i, j] = Convert.ToInt32(MsgBingo2[count]);
+                        count++;
+                    }
+                }
+            }
+        }
+
+        public static void chkBingo(int number)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    // 빙고판에서 해당 번호를 찾아서 0으로 표기
+                    if (Program.bingoA[i, j] == number)
+                    {
+                        Program.bingoA[i, j] = 0;
+                    }
+
+                    if (Program.bingoB[i, j] == number)
+                    {
+                        Program.bingoB[i, j] = 0;
+                    }
+                }
+            }
+        }
+
+        public static string isBingo()
+        {
+            int i, j;
+            int playerA_rowsFlag = 0;
+            int playerA_columnFlag = 0;
+            int playerA_crossleftFlag = 0;
+            int playerA_crossrightFlag = 0;
+            int playerA_check = 0;
+
+            int playerB_rowsFlag = 0;
+            int playerB_columnFlag = 0;
+            int playerB_crossleftFlag = 0;
+            int playerB_crossrightFlag = 0;
+            int playerB_check = 0;
+
+            for (i = 0; i < 5; i++) // // 숫자 체크해서 없애는 부분
+            {
+                playerA_rowsFlag = 0;
+                playerA_columnFlag = 0;
+
+                playerB_rowsFlag = 0;
+                playerB_columnFlag = 0;
+
+                for (j = 0; j < 5; j++) // // bingo[0,0~4] bingo[1,0~4] ...
+                {
+                    if (Program.bingoA[i, j] == 0)
+                    {
+                        playerA_rowsFlag++;
+                    }
+                    if (Program.bingoA[j, i] == 0)
+                    {
+                        playerA_columnFlag++;
+                    }
+
+                    if (Program.bingoB[i, j] == 0)
+                    {
+                        playerB_rowsFlag++;
+                    }
+                    if (Program.bingoB[j, i] == 0)
+                    {
+                        playerB_columnFlag++;
+                    }
+                }
+                // // 빙고 개수 세는 부분
+                // 가로체크
+                if (playerA_rowsFlag == 5)
+                {
+                    playerA_check++;
+                }
+                // 세로체크
+                if (playerA_columnFlag == 5)
+                {
+                    playerA_check++;
+                }
+                // 대각선 왼쪽에서 오른쪽
+                if (Program.bingoA[i, i] == 0)
+                {
+                    playerA_crossleftFlag++;
+                }
+                // 대각선 오른쪽에서 왼쪽
+                if (Program.bingoA[5 - 1 - i, i] == 0)
+                {
+                    playerA_crossrightFlag++;
+                }
+
+                // 가로체크
+                if (playerB_rowsFlag == 5)
+                {
+                    playerB_check++;
+                }
+                // 세로체크
+                if (playerB_columnFlag == 5)
+                {
+                    playerB_check++;
+                }
+                // 대각선 왼쪽에서 오른쪽
+                if (Program.bingoB[i, i] == 0)
+                {
+                    playerB_crossleftFlag++;
+                }
+                // 대각선 오른쪽에서 왼쪽
+                if (Program.bingoB[5 - 1 - i, i] == 0)
+                {
+                    playerB_crossrightFlag++;
+                }
+            }
+
+            if (playerA_crossleftFlag == 5)
+            {
+                playerA_check++;
+            }
+
+            if (playerA_crossrightFlag == 5)
+            {
+                playerA_check++;
+            }
+
+            if (playerB_crossleftFlag == 5)
+            {
+                playerB_check++;
+            }
+
+            if (playerB_crossrightFlag == 5)
+            {
+                playerB_check++;
+            }
+
+            // 새로운 빙고
+            if (playerA_check != Program.playerA_bingoCount)
+            {
+                Console.WriteLine("Game Log: {0}님의 {1}BINGO! [{2}]", Program.playerA, playerA_check, DateTime.Now);
+                Program.playerA_bingoCount++;
+                Program.sendAll("#MSG#|" + Program.playerA + "님의 " + playerA_check + " BINGO!");
+            }
+
+            if (playerB_check != Program.playerB_bingoCount)
+            {
+                Console.WriteLine("Game Log: {0}님의 {1}BINGO! [{2}]", Program.playerB, playerB_check, DateTime.Now);
+                Program.playerB_bingoCount++;
+                Program.sendAll("#MSG#|" + Program.playerB + "님의 " + playerB_check + " BINGO!");
+            }
+
+            // 승리조건
+            if (playerA_check >= 3 && playerB_check < 3)
+            {
+                Console.WriteLine("Game Log: {0}님이 승리했습니다. [{1}]", Program.playerA, DateTime.Now);
+                Program.sendAll("#MSG#|" + Program.playerA + "님의 승리!!");
+                roundClear();
+                return "finish";
+            }
+            else if (playerB_check >= 3 && playerA_check < 3)
+            {
+                Console.WriteLine("Game Log: {0}님이 승리했습니다. [{1}]", Program.playerB, DateTime.Now);
+                Program.sendAll("#MSG#|" + Program.playerB + "님의 승리!!");
+                roundClear();
+                return "finish";
+            }
+            else if (playerA_check >= 3 && playerB_check >= 3)
+            {
+                Console.WriteLine("Game Log: {0}님 {1}님의 공동 승리입니다. [{2}]", Program.playerA, Program.playerB, DateTime.Now);
+                Program.sendAll("#MSG#|" + Program.playerA + ", " + Program.playerB + "님의 공동 승리!!");
+                roundClear();
+                return "finish";
+            }
+            return "continue";
+        }
+
+        public static void roundClear()
+        {
+            Program.sendAll("#GAME-0VER#|");
+
+            // 준비완료된 빙고유저 삭제
+            Program.bingoReadyUser.Remove(Program.playerA);
+            Program.bingoReadyUser.Remove(Program.playerB);
+
+            // 저장된 빙고판 삭제
+            Program.saveUserBingo.Remove(Program.playerA);
+            Program.saveUserBingo.Remove(Program.playerB);
+        }
+        
+        public static void whoFirst()
+        {
+            int attack = 0;
+
+            Random random = new Random();
+            attack = random.Next(0, 100);
+
+
+            // 공격권 부여
+            if (attack % 2 == 0)
+            {
+                string first = Program.userList[0];
+
+                GameManager.copyBingo(first);
+
+                Program.sendAll("#MSG#|" + first + "님이 먼저입니다.");
+                Console.WriteLine("{0}의 선제 공격.", first);
+
+                TcpClient send = Program.bingoReadyUser[first];
+                NetworkStream stream = send.GetStream();
+                StreamWriter sendMsg = new StreamWriter(stream, Encoding.UTF8);
+
+                sendMsg.WriteLine("#Attack#|"); // 메시지 보내기
+                sendMsg.Flush();
+
+                Program.nextUser = Program.userList[1]; // 다음 턴
+
+            }
+            else
+            {
+                string first = Program.userList[1];
+
+                GameManager.copyBingo(first);
+
+                Program.sendAll("#MSG#|" + first + "님이 먼저입니다.");
+                Console.WriteLine("{0}의 선제 공격.", first);
+
+                TcpClient send = Program.bingoReadyUser[first];
+                NetworkStream stream = send.GetStream();
+                StreamWriter sendMsg = new StreamWriter(stream, Encoding.UTF8);
+
+                sendMsg.WriteLine("#Attack#|"); // 메시지 보내기
+                sendMsg.Flush();
+
+                Program.nextUser = Program.userList[0]; // 다음 턴
+            }
+        }
+    }
+}
---
+++ BingoServer/BingoServer/Receiver.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace BingoServer
+{
+    class Receiver
+    {
+        NetworkStream NS = null;
+        StreamReader SR = null;
+        StreamWriter SW = null;
+        TcpClient client;
+
+        public void startClient(TcpClient clientSocket)
+        {
+            client = clientSocket;
+            Thread chat_thread = new Thread(listening); 
+            chat_thread.Start(); 
+        }              
+
+        public void listening()
+        {
+            NS = client.GetStream(); 
+            SR = new StreamReader(NS, Encoding.UTF8); 
+            SW = new StreamWriter(NS, Encoding.UTF8); 
+
+            string GetMessage = string.Empty;
+            string user = string.Empty;
+
+            while (client.Connected == true) 
+            {
+                try
+                {
+                    GetMessage = SR.ReadLine();
+                    string[] MsgResult = GetMessage.Split('|');
+
+                    user = Program.clientList[client];
+
+                    if (GetMessage == "G00D-BY2") 
+                    {
+                        Console.WriteLine("{0}님이 접속을 종료했습니다. ID : {1} [{2}]", ((IPEndPoint)client.Client.RemoteEndPoint).ToString(), user, DateTime.Now);
+                        Program.clientList.Remove(client); // 키 값으로 값 삭제
+                        Program.bingoReadyUser.Remove(user);
+                        Program.saveUserBingo.Remove(user);
+                        if (user == Program.userList[0])
+                        {
+                            Program.userList[0] = "";
+                        }
+                        else
+                        {
+                            Program.userList[1] = "";
+                        }
+                        Console.WriteLine("현재 유저 수 {0}명 [{1}]", Program.clientList.Count, DateTime.Now);
+
+                        if (SW != null)
+                            SW.Close();
+                        if (SR != null)
+                            SR.Close();
+                        if (client != null)
+                            client.Close();
+                        if (NS != null)
+                            NS.Close();
+
+                        if (Program.clientList.Count >= 1)
+                        {
+                            Program.sendAll("#MSG#|" + user + "님이 접속을 종료했습니다.");
+                            Program.sendAll("#GAME-0VER#|");
+                            Program.sendAll("#MSG#|" + user + "님의 퇴장으로 승리하였습니다.");
+                        }
+
+                        break;
+                    }
+                    else if (MsgResult[0] == "#0rder#") // 빙고 번호 교환
+                    {
+                        Console.WriteLine("Game Log: {0}님이 {1}을 불렀습니다. [{2}]", user, MsgResult[1], DateTime.Now);
+                        Program.sendAll(GetMessage);
+                        Program.sendAll("#MSG#|" + user + "님이 " + MsgResult[1] + "을 불렀습니다.");
+
+                        GameManager.chkBingo(Convert.ToInt32(MsgResult[1])); // 눌렀는거 체크
+                        string go = GameManager.isBingo();
+
+                        if (go == "continue")
+                        {                 
+                            string first = Program.nextUser;
+
+                            TcpClient send = Program.bingoReadyUser[first];
+                            NetworkStream stream = send.GetStream();
+                            StreamWriter sendMsg = new StreamWriter(stream, Encoding.UTF8);
+
+                            sendMsg.WriteLine("#Attack#|"); 
+                            sendMsg.Flush();
+
+                            if (Program.nextUser == Program.userList[1]) // 다음 턴
+                            {
+                                Program.nextUser = Program.userList[0];
+                            }
+                            else
+                            {
+                                Program.nextUser = Program.userList[1];
+                            }
+                        }
+                    }
+                    else if (MsgResult[0] == "#ready#") 
+                    {
+                        Console.WriteLine("{0}님이 준비되었습니다. [{1}]", user, DateTime.Now);
+                        Program.sendAll("#MSG#|" + user + "님이 준비되었습니다.");
+
+                        Program.saveUserBingo.Add(user, MsgResult[1]); 
+                        Program.bingoReadyUser.Add(user, client); 
+
+                        if (Program.saveUserBingo.Count >= 2) 
+                        {                            
+                            Console.WriteLine("순서를 결정합니다.");
+                            Program.sendAll("#MSG#|" + "순서를 결정 중 입니다.");
+
+                            GameManager.whoFirst();
+                        }
+
+                    }
+                    else if (MsgResult[0] == "#MSG#") // 채팅
+                    {
+                        Program.sendAll(GetMessage);
+                        Console.WriteLine("Chat Log: {0} - {1} [{2}]", ((IPEndPoint)client.Client.RemoteEndPoint).ToString(), MsgResult[1], DateTime.Now);
+                    }
+                }
+                catch (Exception ee) // 오류
+                {
+                    Program.clientList.Remove(client); 
+                    Program.bingoReadyUser.Remove(user);
+                    Program.saveUserBingo.Remove(user);
+                    if (user == Program.userList[0])
+                    {
+                        Program.userList[0] = "";
+                    }
+                    else
+                    {
+                        Program.userList[1] = "";
+                    }
+
+                    Console.WriteLine("{0}님이 접속을 종료했습니다. ID : {1} [{2}]", ((IPEndPoint)client.Client.RemoteEndPoint).ToString(), user, DateTime.Now);
+                    Console.WriteLine("현재 유저 수 {0}명 [{1}]", Program.clientList.Count, DateTime.Now);
+
+                    if (Program.clientList.Count >= 1)
+                    {
+                        Program.sendAll("#MSG#|" + user + "님이 접속을 종료했습니다.");
+                    }
+
+                    if (SW != null)
+                        SW.Close();
+                    if (SR != null)
+                        SR.Close();
+                    if (client != null)
+                        client.Close();
+                    if (NS != null)
+                        NS.Close();
+
+                    Console.WriteLine("===================================");
+                    Console.WriteLine(ee.ToString());
+                    Console.WriteLine("===================================");
+                }
+            }
+        }
+    }
+}
